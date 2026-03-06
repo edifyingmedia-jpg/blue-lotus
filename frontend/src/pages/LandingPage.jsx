f0b1d1c8c584e5f2c602f44641d6d6c9a80f56f9
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-06 16:53:43 -0500

    Simplify LandingPage component and update content
    Refactored LandingPage component to simplify structure and update content.

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,26 +1,10 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import HeroSection from "../components/HeroSection";
-import FeaturesSection from "../components/FeaturesSection";
-import HowItWorksSection from "../components/HowItWorksSection";
-import WhySection from "../components/WhySection";
-import TestimonialsSection from "../components/TestimonialsSection";
-import CTASection from "../components/CTASection";
-import Footer from "../components/Footer";
 
-const LandingPage = () => {
+export default function LandingPage() {
   return (
-    <div className="min-h-screen bg-gray-950">
-      <Navbar />
-      <HeroSection />
-      <FeaturesSection />
-      <HowItWorksSection />
-      <WhySection />
-      <TestimonialsSection />
-      <CTASection />
-      <Footer />
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Welcome to Blue Lotus</h1>
+      <p>Your creative studio for building emotionally intelligent apps.</p>
     </div>
   );
-};
-
-export default LandingPage;
+}
