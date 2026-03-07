1c2ef0415c043ad9de2882ade037cccabeff6018
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-07 09:32:39 -0500

    Add Settings route and update comments in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import NewProject from "./pages/NewProject";
 import Account from "./pages/Account";
+import Settings from "./pages/Settings";
 import Logout from "./pages/Logout";
 
 export default function App() {
@@ -16,16 +17,14 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/new-project" element={<NewProject />} />
 
-        {/* Account Page */}
+        {/* Account + Settings */}
         <Route path="/account" element={<Account />} />
+        <Route path="/settings" element={<Settings />} />
 
-        {/* Settings Placeholder (build later) */}
-        <Route path="/settings" element={<div>Settings Page Coming Soon</div>} />
-
-        {/* Fully Functional Logout */}
+        {/* Logout */}
         <Route path="/logout" element={<Logout />} />
 
-        {/* Default Route */}
+        {/* Default */}
         <Route path="*" element={<Dashboard />} />
       </Routes>
     </Router>
