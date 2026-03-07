9f812ca2d915ca5005d9d1efb7bae234fc317887
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-07 09:14:52 -0500

    Add Logout page to routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import NewProject from "./pages/NewProject";
+import Logout from "./pages/Logout";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Main Pages */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/new-project" element={<NewProject />} />
 
-        {/* Optional placeholders for future pages */}
+        {/* Placeholder Pages (can be built later) */}
         <Route path="/account" element={<div>Account Page Coming Soon</div>} />
         <Route path="/settings" element={<div>Settings Page Coming Soon</div>} />
-        <Route path="/logout" element={<div>Logout Coming Soon</div>} />
 
-        {/* Default route */}
+        {/* Fully Functional Logout */}
+        <Route path="/logout" element={<Logout />} />
+
+        {/* Default Route */}
         <Route path="*" element={<Dashboard />} />
       </Routes>
     </Router>
