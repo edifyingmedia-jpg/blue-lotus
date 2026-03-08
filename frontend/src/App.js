9d794ee065c635c79a185326445a0b92160080fd
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-08 12:43:54 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,98 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Projects from "./pages/Projects";
-import NewProject from "./pages/NewProject";
-import Account from "./pages/Account";
-import Settings from "./pages/Settings";
-import Logout from "./pages/Logout";
-import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
-import ProjectDetail from "./pages/ProjectDetail";
-import "./styles/animations.css";
+
+import Home from "./pages/Home";
+import ProjectList from "./pages/ProjectList";
+import ProjectDetails from "./pages/ProjectDetails";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Home */}
+        <Route path="/" element={<Home />} />
 
-        {/* Public Route */}
-        <Route path="/login" element={<Login />} />
+        {/* Project List */}
+        <Route path="/projects" element={<ProjectList />} />
 
-        {/* Dashboard */}
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Projects List */}
-        <Route
-          path="/projects"
-          element={
-            <ProtectedRoute>
-              <Projects />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Project Detail (NEW) */}
-        <Route
-          path="/projects/:id"
-          element={
-            <ProtectedRoute>
-              <ProjectDetail />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* New Project */}
-        <Route
-          path="/new-project"
-          element={
-            <ProtectedRoute>
-              <NewProject />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Account */}
-        <Route
-          path="/account"
-          element={
-            <ProtectedRoute>
-              <Account />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Settings */}
-        <Route
-          path="/settings"
-          element={
-            <ProtectedRoute>
-              <Settings />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Logout */}
-        <Route
-          path="/logout"
-          element={
-            <ProtectedRoute>
-              <Logout />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Fallback */}
-        <Route path="*" element={<Login />} />
-
+        {/* Project Details */}
+        <Route path="/projects/:id" element={<ProjectDetails />} />
       </Routes>
     </Router>
   );
 }
+
