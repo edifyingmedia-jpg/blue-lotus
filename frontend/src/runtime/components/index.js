8044ee1a61acb3148f0ab588799e83d0a1a436e9
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-09 20:48:54 -0400

    Update index.js

--- frontend/src/runtime/components/index.js
+++ frontend/src/runtime/components/index.js
@@ -4,12 +4,14 @@ import Text from "./UI/Text";
 import Buttons from "./UI/Buttons";
 import Input from "./UI/Input";
 import Card from "./UI/Card";
+import Image from "./UI/Image";
 
 const ComponentRegistry = {
   // Text component
  Text,
  Input,
  Card,
+ Image,
 
   // Button variants
   PrimaryButton: Buttons.PrimaryButton,
