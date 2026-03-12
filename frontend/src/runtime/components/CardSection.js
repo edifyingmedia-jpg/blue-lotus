88f61c214c0e353dc5d1c313d2c8c14f82ce7ed1
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-12 09:25:50 -0400

    Update CardSection.js

--- frontend/src/runtime/components/CardSection.js
+++ frontend/src/runtime/components/CardSection.js
@@ -1,20 +1,18 @@
 import React from "react";
 
-const CardSection = ({ children, style = {}, ...props }) => {
-  const baseStyle = {
-    padding: 16,
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    gap: 8,
-    ...style,
-  };
-
+export default function CardSection({ children, style, ...props }) {
   return (
-    <div style={baseStyle} {...props}>
+    <div
+      style={{
+        padding: "12px 0",
+        display: "flex",
+        flexDirection: "column",
+        gap: "8px",
+        ...style
+      }}
+      {...props}
+    >
       {children}
     </div>
   );
-};
-
-export default CardSection;
+}
