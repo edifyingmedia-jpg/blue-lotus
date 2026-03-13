be27962eb739e66af621abe3fdfd3e82e4ba3e68
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-13 15:17:01 -0400

    Update index.js

--- frontend/src/runtime/screens/index.js
+++ frontend/src/runtime/screens/index.js
@@ -1,45 +1,32 @@
 // frontend/src/runtime/screens/index.js
 
+import Home from "./Home.json";
+
 /**
  * Screen Registry
  * ---------------------------------------------------------
  * This file defines all screens available in the runtime.
- * Each screen contains a list of components to render.
+ * Each screen is loaded from a JSON file.
  */
 
 const screens = {
-  Home: {
-    components: [
-      {
-        type: "Text",
-        value: "Welcome to Blue Lotus Runtime",
-      },
-      {
-        type: "Button",
-        label: "Go to Next Screen",
-        action: {
-          type: "navigate",
-          screen: "Next",
-        },
-      },
-    ],
-  },
+  Home,
 
   Next: {
     components: [
       {
         type: "Text",
-        value: "You navigated to the next screen!",
+        value: "You navigated to the next screen!"
       },
       {
         type: "Button",
         label: "Go Back",
         action: {
-          type: "goBack",
-        },
-      },
-    ],
-  },
+          type: "goBack"
+        }
+      }
+    ]
+  }
 };
 
 export default screens;
