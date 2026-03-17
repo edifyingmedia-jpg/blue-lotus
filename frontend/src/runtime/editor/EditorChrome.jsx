e472a9389bf4d21fecee5ea54ed45fef772b058d
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-17 09:43:35 -0400

    Update EditorChrome.jsx

--- frontend/src/runtime/editor/EditorChrome.jsx
+++ frontend/src/runtime/editor/EditorChrome.jsx
@@ -4,16 +4,12 @@ import React from "react";
 import LotusCommandPanel from "./LotusCommandPanel";
 import LotusCommandPalette from "./LotusCommandPalette";
 import EditorSurface from "./EditorSurface";
+import { Theme } from "./EditorTheme";
 
 /**
  * EditorChrome
  *
- * Wraps the entire Blue Lotus editor environment:
- * - Command Panel (buttons)
- * - Command Palette (Cmd+K / slash menu)
- * - Editor Surface (textarea + TWINLotus)
- *
- * This is the top-level UI shell for the runtime editor.
+ * The themed UI shell for the Blue Lotus editor.
  */
 
 const EditorChrome = () => {
@@ -25,18 +21,33 @@ const EditorChrome = () => {
         flexDirection: "column",
         width: "100%",
         height: "100%",
-        background: "#000",
-        color: "#eee",
+        background: Theme.colors.bg,
+        color: Theme.colors.text,
+        fontFamily: Theme.fonts.body,
       }}
     >
       {/* Top command bar */}
-      <LotusCommandPanel />
+      <div
+        style={{
+          background: Theme.colors.bgPanel,
+          borderBottom: `1px solid ${Theme.colors.border}`,
+          padding: Theme.spacing.sm,
+        }}
+      >
+        <LotusCommandPanel />
+      </div>
 
       {/* Floating command palette */}
       <LotusCommandPalette />
 
       {/* Main editor */}
-      <div style={{ flex: 1, position: "relative" }}>
+      <div
+        style={{
+          flex: 1,
+          position: "relative",
+          background: Theme.colors.bg,
+        }}
+      >
         <EditorSurface />
       </div>
     </div>
