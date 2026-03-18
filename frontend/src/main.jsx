dfbd7ef86da8f81b2dd9867646ab9fb5b7d61baa
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-18 12:12:31 -0400

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+// frontend/src/main.jsx
+
+/**
+ * main.jsx
+ * --------
+ * Vite entry point.
+ * Renders the Blue Lotus App into the DOM.
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
