7005b4ffaa3f632f1881cbdbd807998487eead5e
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-20 11:44:08 -0400

    Create index.js

---
+++ frontend/src/runtime/navigation/index.js
@@ -0,0 +1,17 @@
+// frontend/src/runtime/navigation/index.js
+
+/**
+ * Navigation Module Exports
+ * ---------------------------------------------------------
+ * Central export surface for the navigation system.
+ * Exposes:
+ * - NavigationContext
+ * - NavigationProvider
+ * - useNavigation
+ */
+
+export {
+  NavigationContext,
+  NavigationProvider,
+  useNavigation,
+} from "./NavigationContext";
