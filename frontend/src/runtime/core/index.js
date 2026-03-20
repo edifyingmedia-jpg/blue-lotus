4f85682f489406b4dc2db4a58fd874b19d81a224
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-20 10:44:20 -0400

    Create index.js

---
+++ frontend/src/runtime/core/index.js
@@ -0,0 +1,6 @@
+// frontend/src/runtime/core/index.js
+
+export { default as createCoreContext } from "./CoreContext.js";
+export { default as CoreEngine } from "./CoreEngine.js";
+export { default as CoreState } from "./CoreState.js";
+export { default as Runtime } from "./Runtime.js";
