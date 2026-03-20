f0d7f92bb554da723ab79acccc9430ea964f6835
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-20 14:58:26 -0400

    Create index.js

---
+++ frontend/src/runtime/index.js
@@ -0,0 +1,15 @@
+// frontend/src/runtime/engine/index.js
+
+/**
+ * Runtime Engine Export Barrel
+ * ---------------------------------------------------------
+ * Centralizes exports for the runtime engine subsystem.
+ *
+ * Allows clean imports like:
+ *
+ *   import { RuntimeEngine } from "@/runtime/engine";
+ */
+
+import RuntimeEngine from "./RuntimeEngine";
+
+export { RuntimeEngine };
