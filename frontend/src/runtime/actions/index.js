1785ce456af3f69bf3c83d9db5e1bafc6c6587f6
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-20 11:57:23 -0400

    Create index.js

---
+++ frontend/src/runtime/actions/index.js
@@ -0,0 +1,13 @@
+// frontend/src/runtime/actions/index.js
+
+/**
+ * Actions Module Exports
+ * ---------------------------------------------------------
+ * Central export surface for the actions system.
+ * Exposes:
+ * - ActionRegistry
+ * - ActionEngine
+ */
+
+export { default as ActionRegistry } from "./ActionRegistry";
+export { default as ActionEngine } from "./ActionEngine";
