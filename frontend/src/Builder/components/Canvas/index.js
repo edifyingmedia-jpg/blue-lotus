f1c5be04df92ba3487d8239e993da148450f0561
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 08:33:55 -0400

    Create index.js

---
+++ frontend/src/Builder/components/Canvas/index.js
@@ -0,0 +1,14 @@
+// frontend/src/Builder/components/Canvas/index.js
+
+/**
+ * Canvas Components Barrel
+ * ---------------------------------------------------------
+ * Central export point for all Canvas-related components.
+ */
+
+export * from "./Canvas";
+// Future Canvas modules will be added here:
+// export * from "./CanvasRenderer";
+// export * from "./CanvasInteractions";
+// export * from "./CanvasSelection";
+// export * from "./CanvasDragDrop";
