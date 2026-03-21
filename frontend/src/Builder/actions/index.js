5c65e89c7ca200e0f5eaa80594f02b903215cdfc
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 07:48:26 -0400

    Create index.js

---
+++ frontend/src/Builder/actions/index.js
@@ -0,0 +1,14 @@
+// frontend/src/Builder/actions/index.js
+
+/**
+ * Builder Actions Barrel
+ * ---------------------------------------------------------
+ * Centralizes all Builder-level actions for clean imports.
+ */
+
+export * from "./exportActions";
+// Future actions will be added here:
+// export * from "./screenActions";
+// export * from "./componentActions";
+// export * from "./runtimeActions";
+// export * from "./aiActions";
