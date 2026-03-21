0342508b5a25ff1e1ee249f1fdd355fd6e6165ec
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 07:55:52 -0400

    Create index.js

---
+++ frontend/src/Builder/components/Toolbar/index.js
@@ -0,0 +1,13 @@
+// frontend/src/Builder/components/Toolbar/index.js
+
+/**
+ * Toolbar Components Barrel
+ * ---------------------------------------------------------
+ * Centralizes exports for all toolbar components.
+ */
+
+export * from "./ExportButton";
+// Future toolbar components will be added here:
+// export * from "./Toolbar";
+// export * from "./ToolbarGroup";
+// export * from "./ToolbarDivider";
