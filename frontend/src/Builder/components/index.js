3066df848b424c32c5953ff30771533e7fc491e0
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 08:10:56 -0400

    Create index.js

---
+++ frontend/src/Builder/components/index.js
@@ -0,0 +1,14 @@
+// frontend/src/Builder/components/index.js
+
+/**
+ * Builder Components Barrel
+ * ---------------------------------------------------------
+ * Central export point for all Builder UI components.
+ */
+
+export * from "./Toolbar";
+// Future Builder components will be added here:
+// export * from "./Canvas";
+// export * from "./Inspector";
+// export * from "./ScreenList";
+// export * from "./ComponentTree";
