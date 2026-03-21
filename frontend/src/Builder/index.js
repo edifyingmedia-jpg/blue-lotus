c1aed77ec162f3965545acf294a4235bea4fac01
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 08:05:17 -0400

    Update index.js

--- frontend/src/Builder/index.js
+++ frontend/src/Builder/index.js
@@ -1,30 +1,12 @@
 // frontend/src/Builder/index.js
 
 /**
- * Builder Export Barrel
+ * Builder Barrel
  * ---------------------------------------------------------
- * Centralizes exports for the Builder subsystem.
- * Allows clean imports like:
- *
- *   import { Builder } from "@/Builder";
+ * Central export point for the Builder subsystem.
  */
 
-import Builder from "./Builder";
-import BuilderEngine from "./BuilderEngine";
-import ProjectLoader from "./ProjectLoader";
-import Sidebar from "./Sidebar";
-import Canvas from "./Canvas";
-import PropertiesPanel from "./PropertiesPanel";
-import ComponentRegistry from "./ComponentRegistry";
-import EditorBridge from "./EditorBridge";
-
-export {
-  Builder,
-  BuilderEngine,
-  ProjectLoader,
-  Sidebar,
-  Canvas,
-  PropertiesPanel,
-  ComponentRegistry,
-  EditorBridge
-};
+export * from "./Builder";
+export * from "./components/Toolbar";
+export * from "./hooks";
+export * from "./actions";
