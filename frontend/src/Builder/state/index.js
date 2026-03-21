b6e9004bcf51f24540dd3001c9e1dac564afde21
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 08:19:14 -0400

    Create index.js

---
+++ frontend/src/Builder/state/index.js
@@ -0,0 +1,13 @@
+// frontend/src/Builder/state/index.js
+
+/**
+ * Builder State Barrel
+ * ---------------------------------------------------------
+ * Central export point for all Builder state hooks.
+ */
+
+export * from "./useBuilderState";
+// Future Builder state modules will be added here:
+// export * from "./useScreenState";
+// export * from "./useComponentState";
+// export * from "./useProjectMetadata";
