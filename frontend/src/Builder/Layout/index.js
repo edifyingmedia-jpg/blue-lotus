01ae21fac9aacee70590ae201f5c73cdf3c94f6f
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 08:27:13 -0400

    Create index.js

---
+++ frontend/src/Builder/Layout/index.js
@@ -0,0 +1,9 @@
+// frontend/src/Builder/Layout/index.js
+
+/**
+ * Builder Layout Barrel
+ * ---------------------------------------------------------
+ * Central export point for all Builder layout components.
+ */
+
+export * from "./BuilderLayout";
