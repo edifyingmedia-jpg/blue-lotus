44a1b7a7b45f08eb128ec7eb4363f9515cc2d6f3
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 08:59:37 -0400

    Update Builder.js

--- frontend/src/Builder/Builder.js
+++ frontend/src/Builder/Builder.js
@@ -4,19 +4,27 @@
  * Builder
  * ---------------------------------------------------------
  * Main Builder container.
- * Wraps the layout and renders the Canvas inside it.
+ * Wires state, actions, and layout together.
  */
 
 import React from "react";
 import { BuilderLayout } from "./Layout";
 import { Canvas } from "./components/Canvas";
 import { useBuilderState } from "./state";
+import { selectScreen } from "./actions/screenActions";
 
 export function Builder({ initialState }) {
-  const { builderState } = useBuilderState(initialState);
+  const { builderState, updateBuilderState } = useBuilderState(initialState);
+
+  const handleSelectScreen = (index) => {
+    selectScreen(updateBuilderState, index);
+  };
 
   return (
-    <BuilderLayout builderState={builderState}>
+    <BuilderLayout
+      builderState={builderState}
+      onSelectScreen={handleSelectScreen}
+    >
       <Canvas builderState={builderState} />
     </BuilderLayout>
   );
