03e64b7d3af148e49eb1f314f5512094aa3fd37d
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 12:39:35 -0400

    Update index.js

--- frontend/src/Builder/index.js
+++ frontend/src/Builder/index.js
@@ -1,12 +1,24 @@
 // frontend/src/Builder/index.js
 
 /**
- * Builder Barrel
+ * Builder Index
  * ---------------------------------------------------------
- * Central export point for the Builder subsystem.
+ * Exports the main Builder entry point and supporting modules.
  */
 
-export * from "./Builder";
-export * from "./components/Toolbar";
-export * from "./hooks";
-export * from "./actions";
+export { default as Builder } from "./Builder";
+export { default as BuilderContext } from "./BuilderContext";
+export { default as BuilderEngine } from "./BuilderEngine";
+export { default as BuilderState } from "./BuilderState";
+export { default as Canvas } from "./Canvas";
+export { default as Sidebar } from "./Sidebar";
+export { default as Toolbar } from "./Toolbar";
+export { default as ScreenRenderer } from "./ScreenRenderer";
+export { default as ComponentRenderer } from "./ComponentRenderer";
+export { default as ComponentRegistry } from "./ComponentRegistry";
+export { default as ActionDispatcher } from "./ActionDispatcher";
+export { default as EditorBridge } from "./EditorBridge";
+export { default as ProjectLoader } from "./ProjectLoader";
+export { default as PropertiesPanel } from "./PropertiesPanel";
+export { default as SceneManager } from "./SceneManager";
+export { default as registerComponents } from "./registerComponents";
