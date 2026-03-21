0594ed4f3b69138eb81f5bde35d3587cae5977d0
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-21 08:44:41 -0400

    Create index.js

---
+++ frontend/src/Builder/components/ScreenList/index.js
@@ -0,0 +1,9 @@
+// frontend/src/Builder/components/ScreenList/index.js
+
+/**
+ * ScreenList Barrel
+ * ---------------------------------------------------------
+ * Central export point for all ScreenList components.
+ */
+
+export * from "./ScreenList";
