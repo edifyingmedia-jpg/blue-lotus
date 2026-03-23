53aa0aa4bab79266bc6d624c7ce1c559556f6966
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-23 17:43:46 -0400

    Update index.js

--- frontend/src/Builder/index.js
+++ frontend/src/Builder/index.js
@@ -1,24 +1,16 @@
-// frontend/src/Builder/index.js
+// index.js
+// Central export hub for the Builder modules
 
-/**
- * Builder Index
- * ---------------------------------------------------------
- * Exports the main Builder entry point and supporting modules.
- */
+export { default as BuilderApp } from "./BuilderApp";
+export { default as BuilderLayout } from "./BuilderLayout";
+export { default as BuilderRouter } from "./BuilderRouter";
 
-export { default as Builder } from "./Builder";
-export { default as BuilderContext } from "./BuilderContext";
-export { default as BuilderEngine } from "./BuilderEngine";
-export { default as BuilderState } from "./BuilderState";
-export { default as Canvas } from "./Canvas";
 export { default as Sidebar } from "./Sidebar";
-export { default as Toolbar } from "./Toolbar";
-export { default as ScreenRenderer } from "./ScreenRenderer";
-export { default as ComponentRenderer } from "./ComponentRenderer";
-export { default as ComponentRegistry } from "./ComponentRegistry";
-export { default as ActionDispatcher } from "./ActionDispatcher";
-export { default as EditorBridge } from "./EditorBridge";
-export { default as ProjectLoader } from "./ProjectLoader";
-export { default as PropertiesPanel } from "./PropertiesPanel";
-export { default as SceneManager } from "./SceneManager";
-export { default as registerComponents } from "./registerComponents";
+export { default as TopBar } from "./TopBar";
+
+export { default as ScreenEditor } from "./ScreenEditor";
+export { default as ComponentEditor } from "./ComponentEditor";
+export { default as NavigationEditor } from "./NavigationEditor";
+export { default as BuilderPreview } from "./BuilderPreview";
+
+export * from "./loadAppDefinition";
