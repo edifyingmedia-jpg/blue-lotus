c833b8733497afcbab643ac3e5793a08d17a8fdb
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-23 17:47:31 -0400

    Update BuilderApp.jsx

--- frontend/src/Builder/BuilderApp.jsx
+++ frontend/src/Builder/BuilderApp.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { BuilderProvider } from "./BuilderContext";
 import BuilderLayout from "./BuilderLayout";
 import BuilderRouter from "./BuilderRouter";
 import "./builder.css";
 
 export default function BuilderApp() {
   return (
-    <div className="builder-app">
-      <BuilderLayout>
-        <BuilderRouter />
-      </BuilderLayout>
-    </div>
+    <BuilderProvider>
+      <div className="builder-app">
+        <BuilderLayout>
+          <BuilderRouter />
+        </BuilderLayout>
+      </div>
+    </BuilderProvider>
   );
 }
