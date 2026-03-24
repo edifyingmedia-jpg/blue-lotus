f24061e1ed17b21f013b470c04d4dad318be49b0
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-24 07:46:05 -0400

    Update index.js

--- frontend/src/runtime/screens/index.js
+++ frontend/src/runtime/screens/index.js
@@ -1,15 +1,25 @@
-// frontend/src/runtime/screens/index.js
-
 /**
  * index.js
- * ---------------------------------------------------------
- * Legacy screen registry placeholder.
+ * ----------------------------------------------------
+ * Public exports for the screens subsystem.
  *
- * In the modern runtime, screens are loaded dynamically
- * through DocumentModel.load(screenName), so this file
- * no longer maintains a registry.
- *
- * It remains only to prevent import errors.
+ * This file provides a clean import surface for:
+ * - DynamicScreen
+ * - LoadScreen
+ * - RenderScreen
+ * - SceneManager
+ * - ScreenRenderer
+ * - ScreenContext
+ * - ScreenEngine
+ * - ScreenLoader
  */
 
-export default {};
+export { default as DynamicScreen } from "./DynamicScreen";
+export { default as LoadScreen } from "./LoadScreen";
+export { default as RenderScreen } from "./RenderScreen";
+export { default as SceneManager } from "./SceneManager";
+export { default as ScreenRenderer } from "./ScreenRenderer";
+export { default as ScreenRendererJSX } from "./ScreenRenderer.jsx";
+export { default as ScreenContext } from "./ScreenContext";
+export { default as ScreenEngine } from "./ScreenEngine";
+export { default as ScreenLoader } from "./ScreenLoader";
