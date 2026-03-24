295d8d065613978eab3704e0ade1757c4a5c0c0d
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-24 07:51:32 -0400

    Update index.js

--- frontend/src/runtime/state/index.js
+++ frontend/src/runtime/state/index.js
@@ -1,21 +1,22 @@
-// frontend/src/runtime/state/index.js
-
 /**
- * State Module Exports
- * ---------------------------------------------------------
- * Central export surface for the runtime state system.
- * Exposes:
- * - StateManager
+ * index.js
+ * ----------------------------------------------------
+ * Public exports for the state subsystem.
+ *
+ * This file provides a clean import surface for:
+ * - StateEngine
+ * - ActionEngine
+ * - Dispatch
  * - Reducer
- * - createDispatch
- * - StateProvider + useStateContext
+ * - StateContext
+ * - StateLoader
+ * - stateManager
  */
 
-export { default as StateManager } from "./StateManager";
+export { default as StateEngine } from "./StateEngine";
+export { default as ActionEngine } from "./ActionEngine";
+export { default as Dispatch } from "./Dispatch";
 export { default as Reducer } from "./Reducer";
-export { default as createDispatch } from "./Dispatch";
-export {
-  StateProvider,
-  useStateContext,
-  StateContext,
-} from "./StateContext";
+export { default as StateContext } from "./StateCotext";
+export { default as StateLoader } from "./StateLoader";
+export { default as stateManager } from "./stateManager";
