4dd91c86aadfd46ec7cfc39c6bf7de6b171e8aed
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-24 06:39:47 -0400

    Update index.js

--- frontend/src/runtime/index.js
+++ frontend/src/runtime/index.js
@@ -1,13 +1,35 @@
-// frontend/src/runtime/index.js
-
 /**
- * Runtime Export Barrel
- * ---------------------------------------------------------
- * Centralizes exports for the entire runtime subsystem.
+ * index.js
+ * ----------------------------------------------------
+ * Central export hub for the Blue Lotus runtime.
+ *
+ * This allows the rest of the application (Builder UI,
+ * Preview, deployed apps) to import runtime modules
+ * cleanly from a single location.
  */
 
-export * from "./engine";
-export * from "./resolver";
-export * from "./state";
-export * from "./view"; // screen runtime engine
-export { default as screens } from "./screens"; // JSON screen registry
+export { default as app } from "./app";
+export { default as api } from "./api";
+export { default as auth } from "./auth";
+
+export { default as RuntimeEngine } from "./RuntimeEngine";
+export { default as RuntimeRenderer } from "./RuntimeRenderer";
+export { default as NavigationEngine } from "./NavigationEngine";
+export { default as StateEngine } from "./StateEngine";
+
+export { default as ScreenRenderer } from "./ScreenRenderer";
+export { default as ScreenRendererJSX } from "./ScreenRenderer.jsx";
+
+export { default as ComponentResolver } from "./ComponentResolver";
+export { default as componentMap } from "./componentMap";
+
+export { default as createBindings } from "./bindings";
+export { default as getFingerprint } from "./fingerprint";
+
+export { default as EventBus } from "./EventBus";
+export { default as ProjectLoader } from "./ProjectLoader";
+export { default as RenderScreen } from "./RenderScreen";
+export { default as SceneManager } from "./SceneManager";
+export { default as LivePreview } from "./LivePreview";
+export { default as PreviewHost } from "./PreviewHost";
+export { default as LoadScreen } from "./LoadScreen";
