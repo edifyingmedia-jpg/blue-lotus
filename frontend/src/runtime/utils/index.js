f48be591617be9d303d5bbac104f8ec4851e961e
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-24 19:31:50 -0400

    Update index.js

--- frontend/src/runtime/utils/index.js
+++ frontend/src/runtime/utils/index.js
@@ -1,19 +1,12 @@
 /**
- * index.js
+ * utils/index.js
  * ----------------------------------------------------
- * Barrel export for all runtime utilities.
- * Keeps imports clean and centralized.
+ * Barrel file for runtime utilities.
  */
 
-export { default as ActionDispatcher } from "./ActionDispatcher";
-export { default as deepClone } from "./deepClone";
-export { default as deepMerge } from "./deepMerge";
-export { default as eventBus } from "./eventBus";
-export { default as format } from "./format";
-export { default as generateId } from "./generateId";
-export { default as isObject } from "./isObject";
-export { default as mergeDeep } from "./mergeDeep";
-export { default as normalizeName } from "./normalizeName";
-export { default as safeGet } from "./safeGet";
-export { default as safeSet } from "./safeSet";
-export { default as validateType } from "./validateType";
+import safeGet from "./safeGet";
+import safeSet from "./safeSet";
+import deepClone from "./deepClone";
+import eventBus from "./eventBus";
+
+export { safeGet, safeSet, deepClone, eventBus };
